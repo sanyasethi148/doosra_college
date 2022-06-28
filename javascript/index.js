a5230d5eb587ef2749d700e282730cee2ca56699
sanya <sanyasethi018@gmail.com>
2022-06-28 19:20:55 +0530

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,111 @@
+
+//const arr = [1, 2, 3, 4, 5, 6];
+//console.log(arr.length);
+
+//console.log(arr[3]);
+
+//arr.forEach((element) => {
+  //  console.log(element);
+ // });
+
+// arr.map((element) => {
+ //   console.log(element);
+ // });
+
+
+ //console.log(array);
+
+ //console.log(new Date());
+
+// console.log(new Date().getTime());
+
+//first
+//const s = "apple";
+
+//console.log(s.substring(0,3));
+
+//data types
+
+//1.primitive- number, string,undefined,null,bigint,symbol,boolean
+//2.non primitive- function , arrays
+
+//console.log(typeof true);
+
+//const object={
+//  name:"sanya",
+//  college:"sgtbimit",
+//}
+//console.log(object.name);
+
+//+ sign always prefer addition operation
+//let s="shivanshu"+10;
+//console.log(s);
+
+//- sign always prefer calculation
+//let m="sanya"-10;
+//console.log(m);
+
+//console.log(typeof NaN); //NaN MEANS NOT A NUMBER
+
+//COERSION(checks data type as well as value)
+//if(1=="1"){
+//  console.log(true);
+//}
+
+//checks data type as well as value
+//if(1==="1"){
+//  console.log(true)
+//}
+//else{
+//  console.log(false)
+//}
+
+//traditional method
+//function apple(){
+//  console.log("apple");
+//}
+//apple();
+
+//arrow method
+// const apple =() => {
+//   console.log("apple");
+// }
+// apple();
+
+//functions
+//functions are vip in javascript
+
+//functions can be assigned to a variable
+//function can be returned from a function
+//function can be passes to another function=callback (most imp)
+
+//1st
+// const variablename = function(){
+//   console.log("apple");
+// };
+// //variablename()
+// variablename("apple");
+
+//2nd
+// var variablename = function(){
+//   return function(){
+//     console.log("apple")
+//   }
+// }
+
+//callback function are the function that passed to another function(most most imp)
+
+//let,const,var
+// const name="apple";  //constant
+// console.log(name);
+
+// var name = "apple"; //for global scope, reassign,redeclare
+// var name;
+// console.log(name);
+
+//let
+// let name ="apple";
+//  name= "mango";
+//  console.log(name);
+
+//camel casing , underscore
